b7c54a4c277ce077e2ae29e8c53e3c9922a74a50
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-20 17:08:16 +0200

    fix #16

--- GroomRoom/GroomRoom/assets/js/main.js
+++ GroomRoom/GroomRoom/assets/js/main.js
@@ -2,3 +2,11 @@
 //SUPER USEFUL STUF IS DONE HERE
 
 var username = "Some douche";
+
+
+$(document).ready(function () {
+
+    $("#logp").text("logged as " + username);
+
+
+});//ready()
---
+++ GroomRoom/GroomRoom/assets/js/responsive.js
@@ -0,0 +1,18 @@
+﻿var popup //The login popup
+
+
+//IS USED TO MAKE A RESPONSIVE DESIGN
+$(document).ready(function () {
+
+    //Popup for login
+    popup = $('[data-remodal-id=modal]').remodal();
+
+    /**
+     * Opens the modal window
+     */
+    popup.open();
+
+    $("#container").css("marginTop", $("header").height().toString() + "px");
+
+
+});
