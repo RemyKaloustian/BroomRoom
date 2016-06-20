ce72ebd6d6adbdd17763e063ab1bda9302f523f6
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-20 15:56:31 +0200

    fix # 7, fix #8, fix #9, fix #10, fix #11

---
+++ GroomRoom/GroomRoom/assets/js/icons.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+
+    var comments_icon = document.getElementById('comments_icon');
+    var comments_options = {
+        from: 'fa-comments',
+        to: 'fa-comments',
+        animation: 'fadeOutTop'
+    };
+
+    var pictures_icon = document.getElementById('pictures_icon');
+    var pictures_options = {
+        from: 'fa-picture-o',
+        to: 'fa-picture-o',
+        animation: 'rollOutLeft'
+    };
+
+    var user_icon = document.getElementById('user_icon');
+    var user_options = {
+        from: 'fa-user',
+        to: 'fa-user',
+        animation: 'rotateClockwise'
+    };
+
+    $("#comments_icon").mouseenter(function () {
+        iconate(comments_icon, comments_options);
+    });
+
+
+    $("#pictures_icon").mouseenter(function () {
+        iconate(pictures_icon, pictures_options);
+    });
+
+    $("#user_icon").mouseenter(function () {
+        iconate(user_icon, user_options);
+    });
+
+
+
+
+});
