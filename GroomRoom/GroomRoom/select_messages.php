e69f948d62ef9e0a157f96974a5d2a75b6c1f69c
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-28 07:57:42 +0200

    Writing the new comment selection request

--- GroomRoom/GroomRoom/select_messages.php
+++ GroomRoom/GroomRoom/select_messages.php
@@ -1,25 +1,51 @@
 ﻿<?php
 
-//SELECTING MESSAGES    
+//SELECTING MESSAGES   
+
+ require 'dbconnect.php';
+
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+    header('Content-type: application/json');
 
 //If values are correctly set
 if($_GET['talk'] != "")
 {
-    mysql_connect("localhost","root","");
-    mysql_select_db("broomroom");
+
+        //NEW STRUCTURE
+        $request = $bdd->prepare('SELECT CONTENT,AUTHOR FROM MESSAGE WHERE TALK = :talk');
+        $request->execute(array("talk"=>$_GET['talk']));
+        $data = $request->fetch();
+            
+        $comments = new stdClass();
+        
+        if(!data)
+        {
+             $reponse = new AJAXAnswer(false, 'aucun nouveaux commentaires');
+             $reponse->answer();
+        }
+        else
+        {
+            
+        }
+
+
+    //BEFORE
+    //mysql_connect("localhost","root","");
+   // mysql_select_db("broomroom");
     //error_reporting(E_ALL && ~E_NOTICE);
    
-    $talk=$_GET['talk'];
+   // $talk=$_GET['talk'];
 
-    $sql="SELECT CONTENT,AUTHOR FROM MESSAGE WHERE TALK = '$talk'";
-    $result=mysql_query($sql);
-    if($result){
+    //$sql="SELECT CONTENT,AUTHOR FROM MESSAGE WHERE TALK = '$talk'";
+  //  $result=mysql_query($sql);
+   // if($result){
     //echo "You have been successfully subscribed.";
-              setcookie("msg","You have been successfully subscribed.",time()+5,"/");
-              header("location:yourphppage.php");
-    }
-     if(!$sql)
-    die(mysql_error());
-    mysql_close();
+           //   setcookie("msg","You have been successfully subscribed.",time()+5,"/");
+            //  header("location:yourphppage.php");
+    //}
+     //if(!$sql)
+   // die(mysql_error());
+   // mysql_close();
 }
 ?>
